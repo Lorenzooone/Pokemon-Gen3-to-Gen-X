616ce2eb53faf927dbc944ef4305aa8145943551
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2022-12-20 02:05:21 +0100

    Implement first gen2 validity checks

--- source/party_handler.c
+++ source/party_handler.c
@@ -2,6 +2,7 @@
 #include "party_handler.h"
 
 u8 get_unown_letter_gen3(u32);
+u8 get_pokemon_gender_kind_gen3(int, u32, u32);
 
 // Order is G A E M, or M E A G reversed. These are their indexes.
 u8 positions[] = {0b11100100, 0b10110100, 0b11011000, 0b10011100, 0b01111000, 0b01101100,
@@ -14,46 +15,68 @@ u8 gender_thresholds_gen12[] = {8, 0, 2, 4, 12, 14, 16, 17};
 
 u8 sprite_counter;
 
+#define MF_7_1_INDEX 2
+#define M_INDEX 1
+#define F_INDEX 6
+#define U_INDEX 7
+#define M_GENDER 0
+#define F_GENDER 1
+#define U_GENDER 2
+#define EGG_FLAG 0x40000000
+#define EGG_NUMBER 412
 #define NAME_SIZE 11
 #define UNOWN_NUMBER 201
 #define UNOWN_B_START 415
+#define UNOWN_EX_LETTER 26
+#define UNOWN_I_LETTER 8
+#define UNOWN_V_LETTER 21
+const u8 pokemon_gender_bin[];
 const u8 pokemon_names_bin[];
 const u8 sprites_cmp_bin[];
 const u8 palettes_references_bin[];
 
-const u8* get_pokemon_name(int index, u32 pid){
+u16 get_mon_index(int index, u32 pid, u32 is_egg){
+    if(is_egg)
+        return EGG_NUMBER;
     if(index != UNOWN_NUMBER)
-        return &(pokemon_names_bin[index*NAME_SIZE]);
-
+        return index;
     u8 letter = get_unown_letter_gen3(pid);
     if(letter == 0)
-        return &(pokemon_names_bin[index*NAME_SIZE]);
-    else
-        return &(pokemon_names_bin[(UNOWN_B_START+letter-1)*NAME_SIZE]);
+        return UNOWN_NUMBER;
+    return UNOWN_B_START+letter-1;
 }
 
-const u8* get_pokemon_sprite_pointer(int index, u32 pid){
+const u8* get_pokemon_name(int index, u32 pid, u32 is_egg){
+    return &(pokemon_names_bin[get_mon_index(index, pid, is_egg)*NAME_SIZE]);
+}
+
+const u8* get_pokemon_sprite_pointer(int index, u32 pid, u32 is_egg){
     u16* sprites_cmp_bin_16 = (u16*)sprites_cmp_bin;
-    u32 position = sprites_cmp_bin_16[0];
-    if(index != UNOWN_NUMBER)
-        return &(sprites_cmp_bin[(sprites_cmp_bin_16[1+index]<<2)+position]);
-
-    u8 letter = get_unown_letter_gen3(pid);
-    if(letter == 0)
-        return &(sprites_cmp_bin[(sprites_cmp_bin_16[1+index]<<2)+position]);
-    else
-        return &(sprites_cmp_bin[(sprites_cmp_bin_16[UNOWN_B_START+letter]<<2)+position]);
+    return &(sprites_cmp_bin[(sprites_cmp_bin_16[1+get_mon_index(index, pid, is_egg)]<<2)+sprites_cmp_bin_16[0]]);
 }
 
-u8 get_palette_references(int index, u32 pid){
-    if(index != UNOWN_NUMBER)
-        return palettes_references_bin[index];
+u8 get_pokemon_gender_gen3(int index, u32 pid, u32 is_egg){
+    u8 gender_kind = get_pokemon_gender_kind_gen3(index, pid, is_egg);
+    switch(gender_kind){
+        case M_INDEX:
+            return M_GENDER;
+        case F_INDEX:
+            return F_GENDER;
+        case U_INDEX:
+            return U_GENDER;
+        default:
+            if((pid & 0xFF) >= gender_thresholds_gen3[gender_kind])
+                return M_GENDER;
+            return F_GENDER;
+    }
+}
 
-    u8 letter = get_unown_letter_gen3(pid);
-    if(letter == 0)
-        return palettes_references_bin[index];
-    else
-        return palettes_references_bin[(UNOWN_B_START+letter-1)];
+u8 get_pokemon_gender_kind_gen3(int index, u32 pid, u32 is_egg){
+    return pokemon_gender_bin[get_mon_index(index, pid, is_egg)];
+}
+
+u8 get_palette_references(int index, u32 pid, u32 is_egg){
+    return palettes_references_bin[get_mon_index(index, pid, is_egg)];
 }
 
 void init_sprite_counter(){
@@ -66,9 +89,9 @@ u8 get_sprite_counter(){
 
 #define OAM 0x7000000
 
-void load_pokemon_sprite(int index, u32 pid){
-    u32 address = get_pokemon_sprite_pointer(index, pid);
-    u8 palette = get_palette_references(index, pid);
+void load_pokemon_sprite(int index, u32 pid, u32 is_egg){
+    u32 address = get_pokemon_sprite_pointer(index, pid, is_egg);
+    u8 palette = get_palette_references(index, pid, is_egg);
     LZ77UnCompVram(address, VRAM+0x10000+(sprite_counter*0x400));
     u16 obj_attr_0 = (SCREEN_HEIGHT - 32);
     u16 obj_attr_1 = (32*sprite_counter)|(1<<15);
@@ -119,8 +142,36 @@ u8 gen3_to_gen2(struct gen2_mon* dst, struct gen3_mon* src) {
     struct gen3_mon_evs* evs = (struct gen3_mon_evs*)&(decryption[3*((positions[index] >> 4)&3)]);
     struct gen3_mon_misc* misc = (struct gen3_mon_misc*)&(decryption[3*((positions[index] >> 6)&3)]);
     
-    iprintf("Species: %s\n", get_pokemon_name(growth->species, src->pid));
-    load_pokemon_sprite(growth->species, src->pid);
+    // Validity checks
+    if(growth->species > LAST_VALID_GEN_2)
+        return 0;
+    
+    // Get shinyness for checks
+    u16 is_shiny = (src->pid & 0xFFFF) ^ (src->pid >> 16) ^ (src->ot_id & 0xFFFF) ^ (src->ot_id >> 16);
+    if(is_shiny < 8)
+        is_shiny = 1;
+    else
+        is_shiny = 0;
+    
+    u8 gender = get_pokemon_gender_gen3(growth->species, src->pid, 0);
+    u8 gender_kind = get_pokemon_gender_kind_gen3(growth->species, src->pid, 0);
+    
+    // These Pokemon cannot be both female and shiny in gen 1/2
+    if(is_shiny && (gender == F_GENDER) && (gender_kind == MF_7_1_INDEX))
+        return 0;
+    
+    // Unown ! and ? did not exist in gen 2
+    // Not only that, but only Unown I and V can be shiny
+    if(growth->species == UNOWN_NUMBER) {
+        u8 letter = get_unown_letter_gen3(src->pid);
+        if(letter >= UNOWN_EX_LETTER)
+            return 0;
+        if(is_shiny && (letter != UNOWN_I_LETTER) && (letter != UNOWN_V_LETTER))
+            return 0;
+    }
+    
+    iprintf("Species: %s\n", get_pokemon_name(growth->species, src->pid, misc->ivs & EGG_FLAG));
+    load_pokemon_sprite(growth->species, src->pid, misc->ivs & EGG_FLAG);
     
     return 1;
 }
