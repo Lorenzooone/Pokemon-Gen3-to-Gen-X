566a1b18a09243305512a5ae80c3aa50bef4d375
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2022-12-29 03:37:57 +0100

    Save a couple of extra bytes

--- source/bin_table_handler.c
+++ source/bin_table_handler.c
@@ -8,13 +8,20 @@ u8* get_table_pointer(u8* table, u16 entry){
     u8 offset_table_pos = 2;
     u8 size_initial_offset = ((table[0] >> 0)&1)+1;
     u8 size_offsets = ((table[0] >> 1)&1)+1;
-    u8 offset_shifter = table[0] >> 2;
+    u8 offset_shifter = (table[0] >> 2) & 7;
+    u8 offset_shifter_initial_offset = (table[0] >> 5);
     if(size_initial_offset == 1)
-        initial_offset = table[1];
+        initial_offset = table[1]<<offset_shifter_initial_offset;
     else {
-        initial_offset = table_16[1];
+        initial_offset = table_16[1]<<offset_shifter_initial_offset;
         offset_table_pos = 4;
     }
+    u16 entries = initial_offset-offset_table_pos;
+    if(size_offsets == 2)
+        entries >>= 1;
+    // Sanity check
+    if(entry >= entries)
+        entry = 0;
     if(size_offsets == 1)
         offset = (table[(offset_table_pos)+entry]) << offset_shifter;
     else
